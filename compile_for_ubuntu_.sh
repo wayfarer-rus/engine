6f5ae04c6da1134284d00d5dc066fcb39b5245e6
Fedor Rusak <fedor.rusak@gmail.com>
2014-11-17 18:37:17 +0400

    Easy build-script fixed

--- compile_for_ubuntu_.sh
+++ compile_for_ubuntu_.sh
@@ -1,1 +1,1 @@
-gcc -o engine main.c luamodule.c graphics.c  -lglfw3 -lXi -lXrandr -lGLU -llua5.2 -I/usr/include/lua5.2 -lopenal -lalut
+gcc -o engine main.c luamodule.c graphics.c alut_module.c gamepad_module.c -lglfw3 -lXi -lXrandr -lGLU -llua5.2 -I/usr/include/lua5.2 -lopenal -lalut
