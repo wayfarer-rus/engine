15e5685d973f2d7765ab182206b693db4b4f45c1
Fedor Rusak <fedor.rusak@gmail.com>
2014-11-17 20:55:19 +0400

    Jump

--- texture_example/texture_load_and_render.c
+++ texture_example/texture_load_and_render.c
@@ -13,6 +13,7 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 	glfwSetWindowShouldClose(window, GL_TRUE);
 	}
+
 int main(void)
 {
 	int width=512,height=512,n=32;
@@ -51,6 +52,8 @@ int main(void)
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+    int base = 0;
+    float delta = 0;
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -65,17 +68,32 @@ int main(void)
 		glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
-		glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+		// glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+
+		base += 1;
+
+		if (base < 30) {
+			delta += 0.01f;
+		}
+		else if (base > 30) {
+			delta -= 0.01f;
+		}
+
+		if (base == 60) {
+			base = 0;
+		}
+		// printf("%i %f\n", base, delta);
+		glTranslatef(-0.5f, -0.7f + delta, 0);
 
 
-		glBegin(GL_TRIANGLES);
-		glColor3f(1.f, 0.f, 0.f);
-		glVertex3f(-0.6f, -0.4f, 0.f);
-		glColor3f(0.f, 1.f, 0.f);
-		glVertex3f(0.6f, -0.4f, 0.f);
-		glColor3f(0.f, 0.f, 1.f);
-		glVertex3f(0.f, 0.6f, 0.f);
-		glEnd();
+		// glBegin(GL_TRIANGLES);
+		// glColor3f(1.f, 0.f, 0.f);
+		// glVertex3f(-0.6f, -0.4f, 0.f);
+		// glColor3f(0.f, 1.f, 0.f);
+		// glVertex3f(0.6f, -0.4f, 0.f);
+		// glColor3f(0.f, 0.f, 1.f);
+		// glVertex3f(0.f, 0.6f, 0.f);
+		// glEnd();
 
 		glColor3f(1,1,1);
 		glEnable(GL_TEXTURE_2D);
@@ -84,10 +102,10 @@ int main(void)
 
 		// Draw a textured quad
 		glBegin(GL_QUADS);
-		glTexCoord2f(0, 0); glVertex3f(0, 0, 0);
-		glTexCoord2f(0, 1.0f); glVertex3f(0, 1.0f, 0);
-		glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f, 1.0f, 0);
-		glTexCoord2f(1.0f, 0); glVertex3f(1.0f, 0, 0);
+		glTexCoord2f(0, 0); glVertex3f(0+delta/2, 1.0f, 0);
+		glTexCoord2f(0, 1.0f); glVertex3f(0+delta/2, 0, 0);
+		glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f-delta/2, 0, 0);
+		glTexCoord2f(1.0f, 0); glVertex3f(1.0f-delta/2, 1.0f, 0);
 		glEnd();
 
 
@@ -96,6 +114,7 @@ int main(void)
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	exit(EXIT_SUCCESS);
