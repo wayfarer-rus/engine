4279d243064843e20124719560c9da426f2225f2
wayfarer-rus <dr.traveller@gmail.com>
2014-11-22 12:42:51 +0300

    pthreads removed from alut_module. They are not necessary there.

--- alut_module.c
+++ alut_module.c
@@ -10,11 +10,6 @@ void alutmodule_init() {
   alGenBuffers(SFX_BUFFER_SIZE, sfxFiles);
   alGenSources(SFX_BUFFER_SIZE, sfxSources);
   sfxIterator = 0;
-
-  int i;
-  for (i = 0; i < SFX_BUFFER_SIZE; ++i) {
-    alutPlayersRunningFlags[i] = false;
-  }
 }
 
 void alutmodule_exit() {
@@ -47,36 +42,15 @@ int alutmodule_loadFile(const char *fileName) {
 }
 
 void alutmodule_playFile (const int id, const float volume, const bool loop) {
-  if (alutPlayersRunningFlags[id])
+  ALuint source = sfxSources[id];
+  ALint status;
+  alGetSourcei(source, AL_SOURCE_STATE, &status);
+
+  if ((status == AL_PLAYING))
     return;
 
-  struct alut_player_attr *player_attr = malloc(sizeof(struct alut_player_attr));
-  player_attr->id = id;
-  player_attr->volume = volume;
-  player_attr->loop = loop;
-
-  printf("Starting alut_player thread: %d\n", id);
-
-  if(pthread_create(&alutPlayers[id], NULL, alut_player, player_attr)) {
-    fprintf(stderr, "Error creating alut_player thread\n");
-    exit(EXIT_FAILURE);
-  }
-  else {
-    alutPlayersRunningFlags[id] = true;
-  }
-}
-
-void *alut_player(void* attr) {
-  assert(attr != NULL);
-  struct alut_player_attr* player_attr = (struct alut_player_attr*)attr;
-  int id = player_attr->id;
-  float volume = player_attr->volume;
-  bool loop = player_attr->loop;
   printf("alut_player: %d | %.2f | %d\n", id, volume, loop);
-  free(player_attr);
-  ALuint source = sfxSources[id];
   ALenum error;
-  ALint status;
 
   alSourcef (source, AL_GAIN, volume/100.0);
   alSourcei (source, AL_LOOPING, loop);
@@ -89,14 +63,4 @@ void *alut_player(void* attr) {
     alutExit ();
     exit (EXIT_FAILURE);
   }
-
-  do
-  {
-    alutSleep(0.001f);
-    alGetSourcei(source, AL_SOURCE_STATE, &status);
-  }
-  while (status == AL_PLAYING);
-
-  alutPlayersRunningFlags[id] = false;
-  pthread_exit(NULL);
 }
--- alut_module.h
+++ alut_module.h
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <pthread.h>
-#include <assert.h>
 #include <AL/alut.h>
 
 #define SFX_BUFFER_SIZE 100
@@ -10,16 +8,12 @@
 static int sfxIterator;
 static ALuint sfxFiles[SFX_BUFFER_SIZE];
 static ALuint sfxSources[SFX_BUFFER_SIZE];
-static pthread_t alutPlayers[SFX_BUFFER_SIZE];
-static bool alutPlayersRunningFlags[SFX_BUFFER_SIZE];
 
 struct alut_player_attr {
   int id;
   float volume;
   bool loop;
 };
-
-static void *alut_player(void* attr);
 
 void alutmodule_init();
 void alutmodule_exit();
